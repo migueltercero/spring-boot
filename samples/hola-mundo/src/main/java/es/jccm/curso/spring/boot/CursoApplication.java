d5977a68da1e9544f8d8f859c026c976753fcc30
migueltercero <miguel.tercero@gmail.com>
2019-04-02 00:23:26 +0200

    subida inicial

--- samples/hola-mundo/src/main/java/es/jccm/curso/spring/boot/CursoApplication.java
+++ samples/hola-mundo/src/main/java/es/jccm/curso/spring/boot/CursoApplication.java
@@ -1,9 +1,12 @@
 package es.jccm.curso.spring.boot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class CursoApplication {
 
 	public static void main(String[] args) {
