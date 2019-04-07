f19ddb29d02cbb1eb378ded5487c194b1f52293b
migueltercero <miguel.tercero@gmail.com>
2019-04-07 18:28:02 +0200

    se actualiza datarest

--- samples/datarest/src/main/java/es/jccm/curso/spring/boot/dao/TipoViaRepository.java
+++ samples/datarest/src/main/java/es/jccm/curso/spring/boot/dao/TipoViaRepository.java
@@ -2,11 +2,11 @@ package es.jccm.curso.spring.boot.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import es.jccm.curso.spring.boot.entity.TipoVia;
 
-@Repository
-public interface TipoViaRepository extends PagingAndSortingRepository<TipoVia, String> {
+@RepositoryRestResource(collectionResourceRel = "tipoVia", path = "tipoVia")
+public interface TipoViaRepository extends PagingAndSortingRepository<TipoVia, Long> {
 	Iterable<TipoVia> findByDisplayContainsIgnoreCase(@Param("display") String display);
 }
