408125a8e33a7afa08385c0743662ea95271791c
migueltercero <miguel.tercero@gmail.com>
2019-04-08 15:38:03 +0200

    software

--- software/choco.bat
+++ software/choco.bat
@@ -1,12 +1,3 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install software.config -y
-
-cd c:\
-git clone https://github.com/migueltercero/spring-boot
-
-cd c:\spring-boot\samples
-mvn clean install -DskipTests
-
-cd c:\spring-boot\software
-java -jar lombok.jar
