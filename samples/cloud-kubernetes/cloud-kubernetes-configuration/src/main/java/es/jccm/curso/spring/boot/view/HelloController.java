4c312109d852a9265c5f63b3c8a80aeea65b9013
migueltercero <miguel.tercero@gmail.com>
2019-04-08 23:19:45 +0200

    cloud kubernetes configuration

---
+++ samples/cloud-kubernetes/cloud-kubernetes-configuration/src/main/java/es/jccm/curso/spring/boot/view/HelloController.java
@@ -0,0 +1,24 @@
+package es.jccm.curso.spring.boot.view;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.jccm.curso.spring.boot.configuration.MyConfig;
+import lombok.extern.slf4j.Slf4j;
+
+
+@RestController
+@Slf4j
+public class HelloController {
+
+	@Autowired
+	private MyConfig myConfig;
+
+	@RequestMapping("/")
+	public String ribbonPing() {
+		log.info(myConfig.getMessage());
+		return myConfig.getMessage();
+	}
+
+}
